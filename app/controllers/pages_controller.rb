6b5dab46852a148ef5920baab10291ef1e191dae
Josue <josuemy@gmail.com>
2017-02-14 14:35:39 -0800

    added method call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     uri.query = URI.encode_www_form({
       "api-key" => "0dca823a2ab54213962f9fd1977c3ea5",
       # "q" => @paramList.first + " " + @paramList.last + " " + @paramList[@paramList.length/2-1] + " " + @paramList[@paramList.length/2] ,
-      "q" => @keywords,
+      "q" => get_upper,
       "sort" => "newest"
     })
     request = Net::HTTP::Get.new(uri.request_uri)
